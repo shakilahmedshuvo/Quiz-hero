ef6de202d4eb0e50992b864144cd96c5ed97dbb7
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-03-15 22:20:09 +0600

    fix start quiz btn to start the quiz

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ startQuiz.addEventListener("click", () => {
 
   let x = setInterval(() => {
     if (counterNum < 0) {
-      coutDown.classList.remove("flex");
-      coutDown.classList.add("hidden");
+      countDown.classList.remove("flex");
+      countDown.classList.add("hidden");
       counterNum = 3;
       count = 0;
       timer = null;
